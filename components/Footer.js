85559f8d322a01829b3365da91976732be350c19
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-10-07 03:42:29 +0300

    Ready to serve

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import footerStyles from '../styles/Footer.module.scss';
 import FooterContact from './FooterContact';
 import FooterNav from './FooterNav';
@@ -8,7 +9,11 @@ function Footer() {
     <div className={footerStyles.footer}>
       <footer>
         <article>
-          <Image src="/logo.svg" alt="RatePunk Logo" width={125} height={32} />
+          <Link href='/'>
+            <a>
+              <Image src="/logo.svg" alt="RatePunk Logo" width={125} height={32} />
+            </a>
+          </Link>
           <p>Ratepunk compares hotel room prices across the major online travel agencies. When you search for a room, Ratepunk extension scans the top booking sites and runs a price comparison, so you can be confident in knowing you’re getting the best deal!</p>
           <description>&copy; 2021 Ratepunk. All Rights Reserved.</description>
         </article>
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import headerStyles from '../styles/Header.module.scss';
 import HeaderNav from './HeaderNav';
 
@@ -6,10 +7,12 @@ function Header() {
   return (
     <div className={headerStyles.header}>
       <header>
-        <a href='/'>
-          <Image style={{ cursor: 'pointer' }} src="/logo.svg"
-            alt="RatePunk Logo" width={125} height={32} />
-        </a>
+        <Link href='/'>
+          <a>
+            <Image style={{ cursor: 'pointer' }} src="/logo.svg"
+              alt="RatePunk Logo" width={125} height={32} />
+          </a>
+        </Link>
         <HeaderNav />
       </header>
     </div>
