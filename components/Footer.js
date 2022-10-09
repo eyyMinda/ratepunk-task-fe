b741ea21374a045d6d91f253f4caff26a65ab4b9
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-10-09 14:08:09 +0300

    Footer fixed

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,8 @@ import Link from 'next/link';
 import footerStyles from '../styles/Footer.module.scss';
 import FooterContact from './FooterContact';
 import FooterNav from './FooterNav';
-import getWidth from '../functions/getWidth';
 
 function Footer() {
-  //getWidth ternary functions not effective!
   return (
     <div className={footerStyles.footer}>
       <footer>
@@ -17,11 +15,10 @@ function Footer() {
             </a>
           </Link>
           <p>Ratepunk compares hotel room prices across the major online travel agencies. When you search for a room, Ratepunk extension scans the top booking sites and runs a price comparison, so you can be confident in knowing you’re getting the best deal!</p>
-          {getWidth() > 768 ? <small className={footerStyles.copy}>&copy; 2021 Ratepunk. All Rights Reserved.</small> : null}
+          <small className={footerStyles.copy}>&copy; 2021 Ratepunk. All Rights Reserved.</small>
         </article>
         <FooterNav />
         <FooterContact />
-        {getWidth() <= 768 ? <small className={footerStyles.copy}>&copy; 2021 Ratepunk. All Rights Reserved.</small> : null}
       </footer>
     </div>
   )
